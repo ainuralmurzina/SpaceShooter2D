38fc90e4c096a043e9bb3ac4697dbee60a85b9d8
BUGMINI-PC\Айнур <aynuralmurzina@gmail.com>
2018-01-29 18:58:57 +0600

    Enemy spawning is created

---
+++ Assets/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject enemy;
+	public int maxEnemies = 5;
+
+	private int counterEnemies;
+	private float maxWidth, maxHeight;
+
+	void Start () {
+		GetScreenMaxParameters ();
+		StartGame ();
+	}
+
+	void GetScreenMaxParameters(){
+		Vector3 currentScreen = Camera.main.ScreenToWorldPoint (new Vector2(Screen.width, Screen.height));
+		maxWidth = currentScreen.x - 1;
+		maxHeight = currentScreen.y + 1;
+	}
+
+	void StartGame(){
+		counterEnemies = 0;
+		StartCoroutine (EnemySpawning());
+	}
+
+	IEnumerator EnemySpawning(){
+
+		while (counterEnemies <= maxEnemies) {
+			Vector2 targetPosition = new Vector2 (Random.Range(-maxWidth, maxWidth), maxHeight);
+
+			Instantiate (enemy, targetPosition, enemy.transform.rotation);
+			counterEnemies++;
+
+			yield return new WaitForSeconds (Random.Range (1f, 5f));
+		}
+		
+	}
+
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,23 +7,37 @@ public class EnemyController : MonoBehaviour {
 	public float speed;
 	public GameObject shot;
 	public Transform shotSpawnPoint;
-	public GameObject explosion;
 
 	private Rigidbody2D myRigidbody;
 	private Vector2 direction;
+	private float maxWidth;
 
 	void Start(){
 		myRigidbody = GetComponent<Rigidbody2D> ();
+
+		GetScreenMaxWidth ();
 
 		StartCoroutine (StartManeuver ());
 		StartCoroutine (StartShoot ());
 
 	}
 
+	void GetScreenMaxWidth(){
+		Vector3 currentScreen = Camera.main.ScreenToWorldPoint (new Vector2(Screen.width, Screen.height));
+		maxWidth = currentScreen.x - 1;
+	}
+
+	void FixedUpdate(){
+		transform.position = new Vector3(
+			Mathf.Clamp (transform.position.x, -maxWidth, maxWidth),
+			transform.position.y, 
+			0f);
+	}
+
 	IEnumerator StartShoot(){
 		while (true) {
 			Instantiate (shot, shotSpawnPoint.position, Quaternion.identity);
-			yield return new WaitForSeconds (Random.Range(0.5f, 2f));
+			yield return new WaitForSeconds (Random.Range(1f, 3f));
 		}
 	}
 
